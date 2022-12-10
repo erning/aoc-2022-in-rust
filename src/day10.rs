a80f3f3117b6746779997d59f5ffd9462dffac78
Zhang Erning <zhang@erning.com>
2022-12-11 01:06:05 +0800

    operator

--- src/day10.rs
+++ src/day10.rs
@@ -75,3 +75,91 @@ mod tests {
         );
     }
 }
+
+//
+//
+//
+
+/*
+#[derive(Debug)]
+enum Operator {
+    Noop,
+    AddX(i32),
+}
+
+fn parse_input(input: &str) -> Vec<Operator> {
+    input
+        .lines()
+        .map(|v| {
+            if v.starts_with("addx") {
+                Operator::AddX(v[5..].parse().unwrap())
+            } else {
+                Operator::Noop
+            }
+        })
+        .collect()
+}
+
+pub fn part_one(input: &str) -> i32 {
+    let mut strengths = 0;
+    let mut cycle = 0;
+    let mut x = 1;
+    for op in parse_input(input) {
+        let mut tick = |c| {
+            for _ in 0..c {
+                cycle += 1;
+                if (cycle + 20) % 40 == 0 {
+                    strengths += cycle * x
+                }
+            }
+        };
+        match op {
+            Operator::Noop => tick(1),
+            Operator::AddX(v) => {
+                tick(2);
+                x += v
+            }
+        }
+    }
+    strengths
+}
+
+pub fn part_two(input: &str) -> String {
+    let mut screen: Vec<char> = vec![];
+    let mut cycle: i32 = 0;
+    let mut x = 1;
+    for op in parse_input(input) {
+        let mut tick = |c| {
+            for _ in 0..c {
+                if (cycle % 40 - x).abs() <= 1 {
+                    screen.push('#');
+                } else {
+                    screen.push(' ');
+                }
+                cycle += 1;
+            }
+        };
+        match op {
+            Operator::Noop => tick(1),
+            Operator::AddX(v) => {
+                tick(2);
+                x += v
+            }
+        }
+    }
+
+    #[cfg(test)]
+    screen.iter_mut().filter(|v| *v == &' ').for_each(|v| *v = '.');
+
+    [
+        String::new(),
+        screen[..]
+            .chunks(40)
+            .map(|v| v.iter().collect())
+            .collect::<Vec<String>>()
+            .join("\n"),
+    ]
+    .join("\n")
+}
+
+*/
