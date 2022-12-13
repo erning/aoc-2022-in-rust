2120392e4759b13d5cff9e815d0649ee7373edc7
Zhang Erning <zhang@erning.com>
2022-12-13 09:29:31 +0800

    part two is simply find shortest path from end position to 'a'

--- src/day12.rs
+++ src/day12.rs
@@ -36,17 +36,23 @@ fn parse_input(input: &str) -> ParsedInput {
         end,
     }
 }
-
-fn dijkstra(
+fn search<F1, F2>(
     heightmap: &Vec<Vec<u8>>,
-    e: (usize, usize),
-    visited: &mut HashSet<(usize, usize)>,
-    queue: &mut BinaryHeap<(i32, (usize, usize))>,
-) -> Option<i32> {
+    s: (usize, usize),
+    is_finish: F1,
+    is_movable: F2,
+) -> Option<i32>
+where
+    F1: Fn((usize, usize)) -> bool,
+    F2: Fn((usize, usize), (usize, usize)) -> bool,
+{
+    let mut visited: HashSet<(usize, usize)> = HashSet::new();
+    let mut queue: BinaryHeap<(i32, (usize, usize))> = BinaryHeap::new();
+    queue.push((0, s));
     let h = heightmap.len() as i32;
     let w = heightmap[0].len() as i32;
     while let Some((step, p)) = queue.pop() {
-        if p == e {
+        if is_finish(p) {
             return Some(-step);
         }
         if !visited.insert(p) {
@@ -61,46 +67,42 @@ fn dijkstra(
             if visited.contains(&np) {
                 continue;
             }
-            if heightmap[np.1][np.0] as i32 - heightmap[p.1][p.0] as i32 > 1 {
-                continue;
+            if is_movable(p, np) {
+                queue.push((step - 1, np));
             }
-            queue.push((step - 1, np));
         }
     }
     None
 }
 
-fn shortest(
-    heightmap: &Vec<Vec<u8>>,
-    s: (usize, usize),
-    e: (usize, usize),
-) -> Option<i32> {
-    let mut visited: HashSet<(usize, usize)> = HashSet::new();
-    let mut queue: BinaryHeap<(i32, (usize, usize))> = BinaryHeap::new();
-    queue.push((0, s));
-    dijkstra(heightmap, e, &mut visited, &mut queue)
-}
-
 pub fn part_one(input: &str) -> i32 {
     let parsed = parse_input(input);
-    shortest(&parsed.heightmap, parsed.start, parsed.end).unwrap()
+    search(
+        &parsed.heightmap,
+        parsed.start,
+        |p| p == parsed.end,
+        |f, t| {
+            parsed.heightmap[t.1][t.0] as i32
+                - parsed.heightmap[f.1][f.0] as i32
+                <= 1
+        },
+    )
+    .unwrap()
 }
 
 pub fn part_two(input: &str) -> i32 {
     let parsed = parse_input(input);
-    let mut starts: Vec<(usize, usize)> = Vec::new();
-    for (y, row) in parsed.heightmap.iter().enumerate() {
-        for (x, v) in row.iter().enumerate() {
-            if v == &b'a' {
-                starts.push((x, y))
-            }
-        }
-    }
-    starts
-        .into_iter()
-        .filter_map(|s| shortest(&parsed.heightmap, s, parsed.end))
-        .min()
-        .unwrap()
+    search(
+        &parsed.heightmap,
+        parsed.end,
+        |p| parsed.heightmap[p.1][p.0] == b'a',
+        |f, t| {
+            parsed.heightmap[f.1][f.0] as i32
+                - parsed.heightmap[t.1][t.0] as i32
+                <= 1
+        },
+    )
+    .unwrap()
 }
 
 #[cfg(test)]
