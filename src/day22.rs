2947f4e29207eb3dae4a31207fa0b44e40912233
Zhang Erning <zhang@erning.com>
2022-12-30 01:41:45 +0800

    part 2 :<

--- src/day22.rs
+++ src/day22.rs
@@ -131,14 +131,113 @@ impl Board {
     }
 }
 
+const CU: i32 = 0;
+const CL: i32 = 1;
+const CF: i32 = 2;
+const CR: i32 = 3;
+const CD: i32 = 4;
+const CB: i32 = 5;
+
 struct Cube {
     surfaces: Vec<Vec<Vec<u8>>>,
+    n: i32,
+    config: Vec<(i32, i32, i32)>,
 }
 
 impl Cube {
     fn new(input: &str) -> Cube {
-        let surfaces = vec![Vec::new(); 6];
-        Cube { surfaces }
+        fn parse_surface(
+            x: i32,
+            y: i32,
+            n: i32,
+            t: i32,
+            board: &Vec<Vec<u8>>,
+        ) -> Vec<Vec<u8>> {
+            let mut surface: Vec<Vec<u8>> = Vec::new();
+            match t {
+                1 => {
+                    for i in 0..=n {
+                        let y = (y + i) as usize;
+                        let mut row: Vec<u8> = Vec::new();
+                        for j in 0..=n {
+                            let x = (x + j) as usize;
+                            row.push(board[y][x]);
+                        }
+                        surface.push(row);
+                    }
+                }
+                2 => {
+                    for i in 0..=n {
+                        let x = (x + i) as usize;
+                        let mut row: Vec<u8> = Vec::new();
+                        for j in 0..=n {
+                            let y = (y + n - j) as usize;
+                            row.push(board[y][x]);
+                        }
+                        surface.push(row);
+                    }
+                }
+                3 => {
+                    for i in 0..=n {
+                        let x = (x + n - i) as usize;
+                        let mut row: Vec<u8> = Vec::new();
+                        for j in 0..=n {
+                            let y = (y + j) as usize;
+                            row.push(board[y][x]);
+                        }
+                        surface.push(row);
+                    }
+                }
+                4 => {
+                    for i in 0..=n {
+                        let y = (y + n - i) as usize;
+                        let mut row: Vec<u8> = Vec::new();
+                        for j in 0..=n {
+                            let x = (x + n - j) as usize;
+                            row.push(board[y][x]);
+                        }
+                        surface.push(row);
+                    }
+                }
+                _ => panic!(),
+            };
+
+            surface
+        }
+
+        let board = Board::new(input);
+
+        let mut n = 4;
+        let mut config = vec![(0, 0, 0); 6];
+
+        config[CU as usize] = (2, 0, 1);
+        config[CB as usize] = (0, 1, 4);
+        config[CL as usize] = (1, 1, 1);
+        config[CF as usize] = (2, 1, 1);
+        config[CD as usize] = (2, 2, 1);
+        config[CR as usize] = (3, 2, 3);
+
+        if board.board[0].len() > 16 {
+            n = 50;
+            config = vec![(0, 0, 0); 6];
+            config[CU as usize] = (1, 0, 1);
+            config[CR as usize] = (2, 0, 2);
+            config[CF as usize] = (1, 1, 1);
+            config[CL as usize] = (0, 2, 2);
+            config[CD as usize] = (1, 2, 1);
+            config[CB as usize] = (0, 3, 3);
+        }
+
+        let mut surfaces = vec![Vec::new(); 6];
+        for (i, &(x, y, t)) in config.iter().enumerate() {
+            surfaces[i] = parse_surface(x * n, y * n, n - 1, t, &board.board);
+        }
+
+        Cube {
+            surfaces,
+            n,
+            config,
+        }
     }
 
     fn next(
@@ -148,11 +247,105 @@ impl Cube {
         y: &mut i32,
         d: &mut i32,
     ) -> bool {
-        true
+        let n = self.surfaces[0][0].len() as i32 - 1;
+
+        let (nx, ny) = match *d {
+            R => (*x + 1, *y),
+            D => (*x, *y + 1),
+            L => (*x - 1, *y),
+            U => (*x, *y - 1),
+            _ => panic!(),
+        };
+
+        if nx >= 0 && nx <= n && ny >= 0 && ny <= n {
+            return if self.is_open_tile(*s, nx, ny) {
+                *x = nx;
+                *y = ny;
+                true
+            } else {
+                false
+            };
+        }
+
+        // cross surfaces
+
+        // standard:
+        //     UUUU
+        //     UUUU
+        //     UUUU
+        //     UUUU
+        // LLLLFFFFRRRR
+        // LLLLFFFFRRRR
+        // LLLLFFFFRRRR
+        // LLLLFFFFRRRR
+        //     DDDD
+        //     DDDD
+        //     DDDD
+        //     DDDD
+        //     BBBB
+        //     BBBB
+        //     BBBB
+        //     BBBB
+
+        let (ns, nd, nx, ny) = match (*s, *d) {
+            (CU, R) => (CR, D, n - *y, 0),
+            (CU, D) => (CF, D, *x, 0),
+            (CU, L) => (CL, D, *y, 0),
+            (CU, U) => (CB, U, *x, n),
+
+            (CL, R) => (CF, R, 0, *y),
+            (CL, D) => (CD, R, 0, n - *x),
+            (CL, L) => (CB, R, 0, n - *y),
+            (CL, U) => (CU, R, 0, *x),
+
+            (CF, R) => (CR, R, 0, *y),
+            (CF, D) => (CD, D, *x, 0),
+            (CF, L) => (CL, L, n, *y),
+            (CF, U) => (CU, U, *x, n),
+
+            (CR, R) => (CB, L, n, n - *y),
+            (CR, D) => (CD, L, n, *x),
+            (CR, L) => (CF, L, n, *y),
+            (CR, U) => (CU, L, n, n - *x),
+
+            (CD, R) => (CR, U, *y, n),
+            (CD, D) => (CB, D, *x, 0),
+            (CD, L) => (CL, U, n - *y, n),
+            (CD, U) => (CF, U, *x, n),
+
+            (CB, R) => (CR, L, n, n - *y),
+            (CB, D) => (CU, D, *x, 0),
+            (CB, L) => (CL, R, 0, n - *y),
+            (CB, U) => (CD, U, *x, n),
+
+            _ => panic!(),
+        };
+
+        if self.is_open_tile(ns, nx, ny) {
+            *s = ns;
+            *x = nx;
+            *y = ny;
+            *d = nd;
+            true
+        } else {
+            false
+        }
     }
 
     fn is_open_tile(&self, s: i32, x: i32, y: i32) -> bool {
         self.surfaces[s as usize][y as usize][x as usize] == 1
+    }
+
+    fn restore(&self, s: i32, x: i32, y: i32, d: i32) -> (i32, i32, i32) {
+        let n = self.n;
+        let (i, j, t) = self.config[s as usize];
+        match t {
+            1 => (i * n + x, j * n + y, d),
+            2 => (i * n + n - y - 1, j * n + x, (d + 3) % 4),
+            3 => (i * n + n - y - 1, j * n + x, (d + 1) % 4),
+            4 => (i * n + n - x - 1, j * n + n - y - 1, (d + 2) % 4),
+            _ => panic!(),
+        }
     }
 }
 
@@ -180,9 +373,32 @@ pub fn part_one(input: &str) -> i32 {
 }
 
 pub fn part_two(input: &str) -> i32 {
-    // let instructions = parse_input(input);
-    // let cube = Cube::new(input);
-    -1
+    let instructions = parse_input(input);
+    let cube = Cube::new(input);
+    // cube.surfaces[CR as usize].iter().for_each(|v| println!("{:?}", v));
+
+    let (mut s, mut x, mut y) = (CU, 0, 0);
+    let mut d = R;
+
+    for i in instructions.into_iter() {
+        if i > 0 {
+            for _ in 0..i {
+                if !cube.next(&mut s, &mut x, &mut y, &mut d) {
+                    break;
+                }
+            }
+        } else {
+            d += i + 2;
+            d += 4;
+            d %= 4;
+        }
+    }
+
+    println!("{:?}", (s, x, y, d));
+    let (x, y, d) = cube.restore(s, x, y, d);
+    println!("{:?}", (x + 1, y + 1, d));
+
+    1000 * (y + 1) + 4 * (x + 1) + d
 }
 
 #[cfg(test)]
@@ -194,6 +410,6 @@ mod tests {
     fn example() {
         let input = read_example(22);
         assert_eq!(part_one(&input), 6032);
-        assert_eq!(part_two(&input), 0);
+        assert_eq!(part_two(&input), 5031);
     }
 }
