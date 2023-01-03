90dff1c7a2fdffbb417de8f93e94e694214173ec
Zhang Erning <zhang@erning.com>
2023-01-03 15:57:23 +0800

    clean day01

--- src/day01.rs
+++ src/day01.rs
@@ -1,30 +1,23 @@
-fn parse_input(input: &str) -> Vec<u32> {
-    let mut calory = 0;
-    let mut calories: Vec<u32> = vec![];
-    for line in input.lines() {
-        match line.parse::<u32>() {
-            Ok(n) => calory += n,
-            Err(_) => {
-                if calory > 0 {
-                    calories.push(calory)
-                }
-                calory = 0
-            }
-        }
-    }
-    if calory > 0 {
-        calories.push(calory);
-    }
-    calories
+fn parse_input(input: &str) -> Vec<Vec<i32>> {
+    input
+        .split("\n\n")
+        .map(|g| g.trim().lines().map(|s| s.parse().unwrap()).collect())
+        .collect()
 }
 
-pub fn part_one(input: &str) -> u32 {
-    let calories = parse_input(input);
-    calories.into_iter().max().unwrap_or(0)
+pub fn part_one(input: &str) -> i32 {
+    parse_input(input)
+        .into_iter()
+        .map(|g| g.into_iter().sum())
+        .max()
+        .unwrap()
 }
 
-pub fn part_two(input: &str) -> u32 {
-    let mut calories = parse_input(input);
+pub fn part_two(input: &str) -> i32 {
+    let mut calories: Vec<i32> = parse_input(input)
+        .into_iter()
+        .map(|g| g.into_iter().sum())
+        .collect();
     calories.sort_unstable();
     calories.into_iter().rev().take(3).sum()
 }
