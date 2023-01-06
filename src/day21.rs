7c45e589c48b71049bd31b0bc04cf3a706092136
Zhang Erning <zhang@erning.com>
2023-01-06 11:27:53 +0800

    clean day21

--- src/day21.rs
+++ src/day21.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+// returns map of (name, job)
 fn parse_input(input: &str) -> HashMap<&str, Vec<&str>> {
     HashMap::from_iter(input.lines().map(|v| {
         let v: Vec<&str> = v.split([' ', ':']).collect();
@@ -8,12 +9,12 @@ fn parse_input(input: &str) -> HashMap<&str, Vec<&str>> {
 }
 
 pub fn part_one(input: &str) -> i64 {
-    fn f(name: &str, monkeys: &HashMap<&str, Vec<&str>>) -> i64 {
-        match monkeys.get(name) {
+    fn f(name: &str, jobs: &HashMap<&str, Vec<&str>>) -> i64 {
+        match jobs.get(name) {
             Some(job) if job.len() == 1 => job[0].parse().unwrap(),
             Some(job) if job.len() == 3 => {
-                let a = f(job[0], monkeys);
-                let b = f(job[2], monkeys);
+                let a = f(job[0], jobs);
+                let b = f(job[2], jobs);
                 match job[1] {
                     "+" => a + b,
                     "-" => a - b,
@@ -26,8 +27,8 @@ pub fn part_one(input: &str) -> i64 {
         }
     }
 
-    let monkeys = parse_input(input);
-    f("root", &monkeys)
+    let jobs = parse_input(input);
+    f("root", &jobs)
 }
 
 pub fn part_two(input: &str) -> i64 {
@@ -35,23 +36,22 @@ pub fn part_two(input: &str) -> i64 {
     // calculate correct part of the tree
     fn p1<'a>(
         name: &'a str,
-        monkeys: &HashMap<&'a str, Vec<&'a str>>,
+        jobs: &HashMap<&'a str, Vec<&'a str>>,
         calculated: &mut HashMap<&'a str, i64>,
     ) -> Option<i64> {
         if name == "humn" {
             return None;
         }
-        match monkeys.get(name) {
+        match jobs.get(name) {
             Some(job) if job.len() == 1 => {
                 let v = job[0].parse().unwrap();
                 calculated.insert(name, v);
                 Some(v)
             }
             Some(job) if job.len() == 3 => {
-                match (
-                    p1(job[0], monkeys, calculated),
-                    p1(job[2], monkeys, calculated),
-                ) {
+                let v1 = p1(job[0], jobs, calculated);
+                let v2 = p1(job[2], jobs, calculated);
+                match (v1, v2) {
                     (Some(a), Some(b)) => {
                         let v = match job[1] {
                             "+" => a + b,
@@ -75,41 +75,42 @@ pub fn part_two(input: &str) -> i64 {
     fn p2(
         name: &str,
         value: i64,
-        monkeys: &HashMap<&str, Vec<&str>>,
+        jobs: &HashMap<&str, Vec<&str>>,
         calculated: &HashMap<&str, i64>,
     ) -> i64 {
         if name == "humn" {
             return value;
         }
-        let job = monkeys.get(name).unwrap();
+        let job = jobs.get(name).unwrap();
         let (a, b) = (job[0], job[2]);
         match (calculated.get(a), calculated.get(b)) {
             (Some(v), None) => match job[1] {
-                "+" => p2(b, value - v, monkeys, calculated),
-                "-" => p2(b, v - value, monkeys, calculated),
-                "*" => p2(b, value / v, monkeys, calculated),
-                "/" => p2(b, v / value, monkeys, calculated),
+                "+" => p2(b, value - v, jobs, calculated),
+                "-" => p2(b, v - value, jobs, calculated),
+                "*" => p2(b, value / v, jobs, calculated),
+                "/" => p2(b, v / value, jobs, calculated),
                 _ => panic!(),
             },
             (None, Some(v)) => match job[1] {
-                "+" => p2(a, value - v, monkeys, calculated),
-                "-" => p2(a, value + v, monkeys, calculated),
-                "*" => p2(a, value / v, monkeys, calculated),
-                "/" => p2(a, value * v, monkeys, calculated),
+                "+" => p2(a, value - v, jobs, calculated),
+                "-" => p2(a, value + v, jobs, calculated),
+                "*" => p2(a, value / v, jobs, calculated),
+                "/" => p2(a, value * v, jobs, calculated),
                 _ => panic!(),
             },
             _ => panic!(),
         }
     }
 
-    let mut monkeys = parse_input(input);
+    let mut jobs = parse_input(input);
     let mut calculated: HashMap<&str, i64> = HashMap::new();
-    p1("root", &monkeys, &mut calculated);
+    p1("root", &jobs, &mut calculated);
 
-    let job = monkeys.get_mut("root").unwrap();
+    let job = jobs.get_mut("root").unwrap();
+    assert_eq!(job.len(), 3);
     // if lhs matchs rhs then lhs - rhs == 0
     job[1] = "-";
-    p2("root", 0, &monkeys, &calculated)
+    p2("root", 0, &jobs, &calculated)
 }
 
 #[cfg(test)]
