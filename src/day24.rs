11f15d9349d54f39c76cc6b46fc7febae627912c
Zhang Erning <zhang@erning.com>
2023-01-06 12:47:58 +0800

    clean day24

--- src/day24.rs
+++ src/day24.rs
@@ -1,8 +1,8 @@
 use std::collections::BinaryHeap;
 use std::collections::HashSet;
 
-type Position = (usize, usize);
-type Blizzard = (Position, usize);
+type Position = (usize, usize); // (x,y)
+type Blizzard = (Position, usize); // (x, y), direction
 const DIRS: [(i32, i32); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
 
 fn parse_input(input: &str) -> (Vec<Blizzard>, usize, usize) {
@@ -29,38 +29,33 @@ fn parse_input(input: &str) -> (Vec<Blizzard>, usize, usize) {
     (blizzards, width, height)
 }
 
-fn next_blizzards(blizzards: &mut [Blizzard], width: usize, height: usize) {
-    for ((x, y), d) in blizzards.iter_mut() {
-        let (dx, dy) = DIRS[*d];
-        *x = (*x as i32 + dx) as usize;
-        if *x == 0 {
-            *x = width - 2;
-        } else if *x == width - 1 {
-            *x = 1;
-        }
-        *y = (*y as i32 + dy) as usize;
-        if *y == 0 {
-            *y = height - 2;
-        } else if *y == height - 1 {
-            *y = 1;
-        }
-    }
-}
-
 fn timed_blizzards(
     mut blizzards: Vec<Blizzard>,
     width: usize,
     height: usize,
 ) -> Vec<HashSet<Position>> {
-    let n = (width - 2) * (height - 2);
-    let mut timed: Vec<HashSet<Position>> = Vec::with_capacity(n);
-    for _ in 0..n {
-        let set: HashSet<Position> =
-            HashSet::from_iter(blizzards.iter().map(|&(p, _)| p));
-        timed.push(set);
-        next_blizzards(&mut blizzards, width, height);
-    }
-    timed
+    (0..(width - 2) * (height - 2))
+        .map(|_| {
+            let v = HashSet::from_iter(blizzards.iter().map(|&(p, _)| p));
+            // next blizzards
+            for ((x, y), d) in blizzards.iter_mut() {
+                let (dx, dy) = DIRS[*d];
+                *x = (*x as i32 + dx) as usize;
+                if *x == 0 {
+                    *x = width - 2;
+                } else if *x == width - 1 {
+                    *x = 1;
+                }
+                *y = (*y as i32 + dy) as usize;
+                if *y == 0 {
+                    *y = height - 2;
+                } else if *y == height - 1 {
+                    *y = 1;
+                }
+            }
+            v
+        })
+        .collect()
 }
 
 fn next_time(
@@ -89,7 +84,7 @@ fn distance(a: Position, b: Position) -> usize {
         as usize
 }
 
-fn trip(
+fn search(
     start: Position,
     time: usize,
     target: Position,
@@ -97,22 +92,20 @@ fn trip(
     height: usize,
     timed: &[HashSet<Position>],
 ) -> usize {
-    let heuristic =
-        |p: Position, t: usize| -((t + distance(p, target)) as i32);
+    let heuristic = |p: Position| -(distance(p, target) as i32);
 
     let mut visited: HashSet<(Position, usize)> = HashSet::new();
     let mut queue: BinaryHeap<(i32, Position, usize)> = BinaryHeap::new();
-    queue.push((heuristic(start, time), start, time));
+    queue.push((heuristic(start), start, time));
 
     while let Some((_, (x, y), t)) = queue.pop() {
         if (x, y) == target {
             return t;
         }
 
-        if visited.contains(&((x, y), t)) {
+        if !visited.insert(((x, y), t)) {
             continue;
         }
-        visited.insert(((x, y), t));
 
         for (dx, dy) in DIRS {
             let (nx, ny) = (x as i32 + dx, y as i32 + dy);
@@ -125,7 +118,7 @@ fn trip(
             }
             let p = (nx as usize, ny as usize);
             for nt in next_time(t, (x, y), p, timed) {
-                queue.push((heuristic(p, nt + t), p, nt));
+                queue.push((heuristic(p) - (nt + t) as i32, p, nt));
             }
         }
     }
@@ -140,7 +133,7 @@ pub fn part_one(input: &str) -> usize {
     let start = (1, 0);
     let target = (width - 2, height - 2);
 
-    trip(start, 0, target, width, height, &timed) + 1
+    search(start, 0, target, width, height, &timed) + 1
 }
 
 pub fn part_two(input: &str) -> usize {
@@ -152,9 +145,9 @@ pub fn part_two(input: &str) -> usize {
     let c = (width - 2, height - 1);
     let d = (1, 1);
 
-    let t = trip(a, 0, b, width, height, &timed) + 1;
-    let t = trip(c, t, d, width, height, &timed) + 1;
-    trip(a, t, b, width, height, &timed) + 1
+    let t = search(a, 0, b, width, height, &timed) + 1;
+    let t = search(c, t, d, width, height, &timed) + 1;
+    search(a, t, b, width, height, &timed) + 1
 }
 
 #[cfg(test)]
